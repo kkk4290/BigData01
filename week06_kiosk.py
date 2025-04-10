998e80cf79f9beea26710826fc327000db1a649d
zurr <202330307@daelim.ac.kr>
2025-04-10 10:34:47 +0900

    v0.4 kiosk

--- week06_kiosk.py
+++ week06_kiosk.py
@@ -1,8 +1,8 @@
 # 1) 아아: 2000원 2) 라떼: 2500원
-
+drinks = ["아이스 아메리카노", "카페라떼"]
 prices = [2000, 2500]
 while True:
-    menu = input(f"1) 아이스 아메리카노 {prices[0]}2) 카페라떼 {prices[1]}3) 주문종료:")
+    menu = input(f"1) {drinks[0]} {prices[0]}2) {drinks[1]}{prices[1]}3) 주문종료:")
     if menu == "1":
         print(f"아이스 아메리카노를 주문하셨습니다. 가격은 {prices[0]}원 입니다")
     if menu == "2":
