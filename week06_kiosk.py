452f9a672bb48b2d375f233c521694df9c67d1bb
zurr <202330307@daelim.ac.kr>
2025-04-10 10:39:32 +0900

    v0.5 kiosk

--- week06_kiosk.py
+++ week06_kiosk.py
@@ -1,12 +1,19 @@
 # 1) 아아: 2000원 2) 라떼: 2500원
 drinks = ["아이스 아메리카노", "카페라떼"]
 prices = [2000, 2500]
+total_price = 0
 while True:
     menu = input(f"1) {drinks[0]} {prices[0]}2) {drinks[1]}{prices[1]}3) 주문종료:")
     if menu == "1":
         print(f"아이스 아메리카노를 주문하셨습니다. 가격은 {prices[0]}원 입니다")
-    if menu == "2":
+        total_price = total_price + prices[0]
+    else if menu == "2":
         print(f"카페라떼를 주문하셨습니다. 가격은 {prices[1]}원 입니다")
-    if menu == "1":
+        total_price = total_price + prices[1]
+    else if menu == "3":
         print("주문을 종료합니다")
         break
+    else:
+        print(f"{menu}번 메뉴는 존재하지 않습니다. 아래메누에서 골라주세요")
+
+print(f"총 주문 금액: {total_price[}원")
