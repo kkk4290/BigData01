e3378a8117f8b4fccdb6b65a3e6c788bf3062227
zurr <202330307@daelim.ac.kr>
2025-04-17 11:27:52 +0900

    v1.9 kiosk

--- week07_kiosk.py
+++ week07_kiosk.py
@@ -28,6 +28,17 @@ def display_menu():
     menu_texts = menu_texts + f"{len(drinks)+1}) 주문종료 : "
     return menu_texts
 
+def print_receipt():
+    """
+    영수증 출력 가능
+    :return: 없음
+    """
+    print(f"{'상품명':^20}{'단가':^6}{'수량':^6}{'금액':^6}")
+    for i in range(len(drinks)):
+        if amounts[i] > 0:
+            print(f"{drinks[i]:^20}{prices[i]:^6}{amounts[i]:^6}{prices[i] * amounts[i]:^6}")
+
+    print(f"총 주문 금액 : {total_price}원")
 
 while True:
     menu = int(input(display_menu()))
@@ -39,9 +50,3 @@ while True:
     else:
         print(f"{menu}번 메뉴는 존재하지 않습니다. 아래 메뉴에서 골라주세요")
 
-print(f"{'상품명':^20}{'단가':^6}{'수량':^6}{'금액':^6}")
-for i in range(len(drinks)):
-    if amounts[i] > 0:
-        print(f"{drinks[i]:^20}{prices[i]:^6}{amounts[i]:^6}{prices[i] * amounts[i]:^6}")
-
-print(f"총 주문 금액 : {total_price}원")
