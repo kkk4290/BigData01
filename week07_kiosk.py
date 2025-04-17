786cd590b5b1d86cd61ade5441de3433e79fe7b5
zurr <202330307@daelim.ac.kr>
2025-04-17 11:33:21 +0900

    v2.0 kiosk

--- week07_kiosk.py
+++ week07_kiosk.py
@@ -6,7 +6,7 @@ prices = [1500, 2500, 4000, 4200]
 total_price = 0
 amounts = [0] * len(drinks)  # amounts = [0 for _ in range(len(drinks))]
 
-def order_process(idx):
+def order_process(idx: int) -> None:
     """
     주문 처리 함수 1) 주문 디스플레이  2) 총 주문 금액 누산  3) 주문 품목 수량 업데이트
     :param idx: 고객이 선택한 메뉴 - 1 (인덱스, 정수)
@@ -18,7 +18,7 @@ def order_process(idx):
     amounts[idx] = amounts[idx] + 1
 
 
-def display_menu():
+def display_menu() -> str:
     """
     음료 선택 메뉴 디스플레이 기능
     :return: 음료 메뉴 및 주문 종료 문자열
@@ -28,7 +28,7 @@ def display_menu():
     menu_texts = menu_texts + f"{len(drinks)+1}) 주문종료 : "
     return menu_texts
 
-def print_receipt():
+def print_receipt() -> None:
     """
     영수증 출력 가능
     :return: 없음
@@ -39,6 +39,7 @@ def print_receipt():
             print(f"{drinks[i]:^20}{prices[i]:^6}{amounts[i]:^6}{prices[i] * amounts[i]:^6}")
 
     print(f"총 주문 금액 : {total_price}원")
+
 
 while True:
     menu = int(input(display_menu()))
