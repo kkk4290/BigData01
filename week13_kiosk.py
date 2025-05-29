f2d4390d54d87a518a9f6b2fe341cef3538f73f7
zurr <202330307@daelim.ac.kr>
2025-05-29 10:39:10 +0900

    v3.9 kiosk

--- week13_kiosk.py
+++ week13_kiosk.py
@@ -3,9 +3,13 @@ import kiosk as kk
 import requests
 
 if __name__ == "__main__":
-    url = f"http://wttr.in/suwon?format=%C+%t&lang=ko"
+    #url = f"http://wttr.in/suwon?format=%C+%t&lang=ko"
+    url = f"http://naver.com/kim"  #404 page not found
     response = requests.get(url)
-    print(response.text.strip())
+    if response.status_code == 200:
+        print(response.text.strip())
+    else:
+        print(f"상태코드: {response.status_code}")
     kk.run()
     kk.print_receipt()
     kk.get_ticket_number()
